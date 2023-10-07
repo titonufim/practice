4df40eddf5bc4584660efb4b89643125e04ceab8
Kuzmin <titonufim@mail.ru>
2023-10-08 02:31:16 +0400

    test Identity

--- lr2/src/main/java/functions/IdentityFunction.java
+++ lr2/src/main/java/functions/IdentityFunction.java
@@ -5,3 +5,4 @@ public class IdentityFunction  implements MathFunction{
         return x;
     }
 }
+
---
+++ lr2/src/test/java/functions/IdentityFunctionTest.java
@@ -0,0 +1,18 @@
+package functions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentityFunctionTest {
+    public void testApply() {
+    MathFunction IdentityFunction = new IdentityFunction();
+
+        double result = IdentityFunction.apply(5.0);
+        System.out.println(result + "expected: 5.0");
+
+        result = IdentityFunction.apply(-3.0);
+        System.out.println(result + "expected: -3.0");
+
+        result = IdentityFunction.apply(0.0);
+        System.out.println(result + "expected: 0");
+    }
+}
