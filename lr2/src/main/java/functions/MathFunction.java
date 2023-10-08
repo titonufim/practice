aea28795ff0c45ee340b93e2a4605ee7f9c56835
Dubyan <dubyansky.exe@gmail.com>
2023-10-08 18:26:52 +0300

    andThen + finish task1

--- lr2/src/main/java/functions/MathFunction.java
+++ lr2/src/main/java/functions/MathFunction.java
@@ -2,4 +2,7 @@ package functions;
 
 public interface MathFunction {
     double apply(double x);
+    default MathFunction andThen(MathFunction afterFunction) {
+        return x -> afterFunction.apply(apply(x));
+    }
 }
---
+++ lr2/src/test/java/functions/MathFunctionTest.java
@@ -0,0 +1,35 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathFunctionTest {
+
+    @Test
+    void testAndThen() {
+        MathFunction addOne = x -> x + 1;
+        MathFunction square = x -> x * x;
+
+        MathFunction addOneAndSquare = addOne.andThen(square);
+
+        double result = addOneAndSquare.apply(2);
+        assertEquals(9, result);
+
+        result = addOneAndSquare.apply(-3);
+        assertEquals(16, result);
+
+        MathFunction multiplyByTwo = x -> x * 2;
+        MathFunction addFive = x -> x + 5;
+        MathFunction subtractThree = x -> x - 3;
+
+        MathFunction combinedFunction = multiplyByTwo.andThen(addFive).andThen(subtractThree);
+
+        result = combinedFunction.apply(4);
+        assertEquals(13, result);
+
+
+        result = combinedFunction.apply(-2);
+        assertEquals(1, result);
+    }
+}
