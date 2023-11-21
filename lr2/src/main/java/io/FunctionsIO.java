84eef4b58c44f5381bcb72e81d531fc890800f2f
Kuzmin <titonufim@mail.ru>
2023-11-21 05:20:04 +0400

    Lr5 LinkedListTabulatedTabulatedFunction

--- lr2/src/main/java/io/FunctionsIO.java
+++ lr2/src/main/java/io/FunctionsIO.java
@@ -29,12 +29,6 @@ public final class FunctionsIO {
             e.printStackTrace();
         }
     }
-    public static void serialize(BufferedOutputStream stream, TabulatedFunction function) throws IOException {
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(stream);
-        objectOutputStream.writeObject(function);
-        objectOutputStream.flush();
-    }
-
     public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
         try {
             int count = Integer.parseInt(reader.readLine());
@@ -83,10 +77,14 @@ public final class FunctionsIO {
         return factory.create(xValue, yValue);
     }
 
-    public static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(stream)) {
-            return (TabulatedFunction) objectInputStream.readObject();
-        }
+    public static void serialize(BufferedOutputStream stream, TabulatedFunction function) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(stream);
+        objectOutputStream.writeObject(function);
+        objectOutputStream.flush();
+    }
+    public static  TabulatedFunction deserialize(BufferedInputStream stream) throws IOException,ClassNotFoundException{
+        ObjectInputStream objectInputStream = new ObjectInputStream(stream);
+        return (TabulatedFunction)objectInputStream.readObject();
     }
 
 }
