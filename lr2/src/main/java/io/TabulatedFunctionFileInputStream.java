ed826f0d0f01b1b57a811e97be3f34b0adeafe9c
Dubyan <dubyansky.exe@gmail.com>
2023-11-21 04:44:30 +0300

    lr5

--- lr2/src/main/java/io/TabulatedFunctionFileInputStream.java
+++ lr2/src/main/java/io/TabulatedFunctionFileInputStream.java
@@ -2,30 +2,30 @@ package io;
 
 import functions.*;
 import functions.factory.*;
-import operations.*;
-
 import java.io.*;
-
+import operations.*;
 public class TabulatedFunctionFileInputStream {
     public static void main(String[] args) {
-        try (BufferedInputStream fileInputStream = new BufferedInputStream(new FileInputStream("lr2/input/binary function.bin"))) {
+        try (BufferedInputStream fileInput = new BufferedInputStream(new FileInputStream("lr2/input/binary function.bin")) ) {
             TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
-            TabulatedFunction function = FunctionsIO.readTabulatedFunction(fileInputStream, factory);
-            System.out.println("Считанная функция:");
-            System.out.println(function.toString());
+            TabulatedFunction function = FunctionsIO.readTabulatedFunction(fileInput, factory);
+            System.out.println(function);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try {
-            System.out.println("Введите размер и значения функции:");
+
+        try{ BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Введите размер и значения функции: ");
             TabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
-            TabulatedFunction function = FunctionsIO.readTabulatedFunction(new BufferedInputStream(System.in), factory);
-            System.out.println("Производная функции:");
-            TabulatedDifferentialOperator differentialOperator = new TabulatedDifferentialOperator(factory);
-            TabulatedFunction derivative = differentialOperator.derive(function);
-            System.out.println(derivative.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
+            TabulatedDifferentialOperator operator = new TabulatedDifferentialOperator(factory);
+            TabulatedFunction function = FunctionsIO.readTabulatedFunction(inputReader, factory);
+            System.out.println(operator.derive(function));
         }
+
+        catch  (IOException e) {
+            e.printStackTrace();}
+
     }
 }
