073165a80df79d85a93df6d073958395d1c75a96
Kuzmin <titonufim@gmail.com>
2023-12-03 21:50:20 +0500

    LR6 WriteTask

--- lr2/src/main/java/concurrent/WriteTask.java
+++ lr2/src/main/java/concurrent/WriteTask.java
@@ -1,4 +1,22 @@
 package concurrent;
 
-public class WriteTask {
+import functions.*;
+
+public class WriteTask implements Runnable {
+    private TabulatedFunction function;
+    private double value;
+    public WriteTask(TabulatedFunction function, double value) {
+        this.function = function;
+        this.value = value;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+                function.setY(i, value);
+                System.out.printf("Writing for index %d complete%n", i);
+            }
+        }
+
+    }
+
 }
