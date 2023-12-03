b69217a27a4660a5aade4cfdc9334d2d7f0453ac
Kuzmin <titonufim@gmail.com>
2023-12-03 21:32:46 +0500

    LR6 Readtask

---
+++ lr2/src/main/java/concurrent/ReadTask.java
@@ -0,0 +1,17 @@
+package concurrent;
+
+import functions.*;
+
+public class ReadTask implements Runnable {
+    private TabulatedFunction function;
+
+    public ReadTask(TabulatedFunction function) {
+        this.function = function;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); ++i) {
+            System.out.printf("After read: i = %d, x = %f, y = %f%n", i, function.getX(i), function.getY(i));
+        }
+    }
+}
